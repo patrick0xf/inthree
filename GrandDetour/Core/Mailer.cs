0227a2a100791834e6df5b1f6dcebcb5ae1cc330
Patrick F <patrick0xf@thunderground.net>
2020-05-12 18:30:47 -0700

    These files may need a security overhaul

---
+++ GrandDetour/Core/Mailer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace GrandDetour.Core
+{
+    public static class Mailer
+    {
+        private static SmtpClient _smtpClient;
+        private static SmtpClient _smtpClientSupport;
+        private static readonly MailAddress EcoConcierge = new MailAddress("emailfromemail".AsSetting(), "emailfromname".AsSetting());
+        private static readonly MailAddress SupportPerson = new MailAddress("supportemailfromemail".AsSetting(), "supportemailfromname".AsSetting());
+
+        public static string SendNotificationMail(string recipient, string subject, string body)
+        {
+            string error = null;
+
+            try
+            {
+                NotificationSmtpClient.Send(new MailMessage(EcoConcierge, new MailAddress(recipient)) { Subject = subject, Body = body });
+            }
+            catch(Exception ex)
+            {
+                error = ex.InnerExceptionMessage();
+            }
+
+            return error;
+        }
+
+        public static string SendSupportMail(string recipient, string subject, string body)
+        {
+            string error = null;
+
+            try
+            {
+                SupportSmtpClient.Send(new MailMessage(EcoConcierge, new MailAddress(recipient)) { Subject = subject, Body = body });
+            }
+            catch (Exception ex)
+            {
+                error = ex.InnerExceptionMessage();
+            }
+
+            return error;
+        }
+
+        private static SmtpClient NotificationSmtpClient
+        {
+            get
+            {
+                if (_smtpClient == null)
+                {
+                    _smtpClient = new SmtpClient("mailserver".AsSetting(), "mailport".AsSetting().ToSafeInt(25));
+
+                    if (_smtpClient.Port == 587)
+                    {
+                        _smtpClient.EnableSsl = true;
+                        ServicePointManager.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
+                    }
+
+                    var emailLogin = "emaillogin".AsSetting();
+                    var emailPassword = "emailpassword".AsSetting();
+
+                    if (!String.IsNullOrEmpty(emailLogin) || !String.IsNullOrEmpty(emailPassword))
+                    {
+                        _smtpClient.UseDefaultCredentials = false;
+                        _smtpClient.Credentials = new NetworkCredential(emailLogin, emailPassword);
+                    }
+                }
+
+                return _smtpClient;
+            }
+        }
+
+        private static SmtpClient SupportSmtpClient
+        {
+            get
+            {
+                if (_smtpClientSupport == null)
+                {
+                    //Not implemented
+                    _smtpClientSupport = null;
+                }
+                return _smtpClientSupport;
+            }
+        }
+    }
+}
---
+++ GrandDetour/backspace/login.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using GrandDetour.Core;
+
+namespace GrandDetour.Backspace
+{
+    public partial class Login : AccountPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Request.Cookies.Clear();
+        }
+
+        protected void BtnLoginClick(object sender, EventArgs e)
+        {
+            if (IsIPLocked(SiteName.Backspace, Request.UserHostAddress))
+            {
+                txtPassword.Text = String.Empty;
+                txtPassword.Focus();
+            }
+
+            //TODO this needs to be re-wired somewhere
+            if(txtLogin.Text.Hash() == "admin" &&
+                txtPassword.Text.Hash() == "admin")
+            {
+                Response.Cookies.Add(GetAdminLoginCookie(Request.Cookies["token"] ?? new HttpCookie("token")));
+                Response.Redirect("home.aspx");
+            }
+            else
+            {
+                txtPassword.Text = String.Empty;
+                txtPassword.Focus();
+            }
+        }
+
+        private static HttpCookie GetAdminLoginCookie(HttpCookie cookie)
+        {
+            cookie.Value = DateTime.Now.HourlyToken("administrator");
+            cookie.Expires = DateTime.Now.AddDays(1);
+            return cookie;
+        }
+    }
+}
